d95ff4133f6b931c2407a01c184eaf96a416f9d2
Vova <you@example.com>
2014-11-05 10:59:58 +0200

    fabfile and req added

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import put, run
+from fabvenv import Venv
+
+ROOT = "/opt/lv128/WebClient/"
+
+def deploy():
+    venv = Venv(ROOT, "requirements.txt")
+    if not venv.exists():
+        venv.create()
+    venv.install()
+    put("server.py", ROOT)
+    put("web_cl.service", ROOT)
+    run("sudo mv %s/web_cl.service /etc/systemd/system/" % ROOT)
+    run("sudo systemctl enable web_cl")
+    run("sudo systemctl restart web_cl")
