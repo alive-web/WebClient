e6f177c05a56b728cf3574db65244000ea23984a
Vova <you@example.com>
2014-11-05 20:08:10 +0200

    added files

--- fabfile.py
+++ fabfile.py
@@ -8,8 +8,10 @@ def deploy():
     if not venv.exists():
         venv.create()
     venv.install()
-    put("index.html", ROOT)
+    put("server.py", ROOT)
     put("web_cl.service", ROOT)
+    put("index.html", ROOT)
+    put("script.js", ROOT)
     run("sudo mv %s/web_cl.service /etc/systemd/system/" % ROOT)
     run("sudo systemctl enable web_cl")
     run("sudo systemctl restart web_cl")
