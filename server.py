e66077a5637cd197f0d7edb5c31666ffe4777280
Volodymyr <web.switch.ua@gmail.com>
2014-11-07 10:52:38 +0200

    Log added

--- server.py
+++ server.py
@@ -2,20 +2,33 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import logging
+
+log.getLogger('log')
+log_file = logging.FileHandler('log.log')
+log_file.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+log_file.setFormatter(formatter)
+log.addHandler(log_file)
+
 
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
-        print "WebSocket opened"
+        log.info('WebSocket opened' + self.current_user )
+        
 
     def on_message(self, message):
         self.write_message(message)
 
     def on_close(self):
-        print "WebSocket closed"
+        
 application = tornado.web.Application([
     (r"/", EchoWebSocket),
 ])
 
+
+    
 if __name__ == "__main__":
+    
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
