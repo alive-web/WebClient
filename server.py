79082731abec45bf78029f2261dce2377c916ea0
Volodymyr <web.switch.ua@gmail.com>
2014-11-07 11:04:21 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,24 +4,19 @@ import tornado.web
 import tornado.websocket
 import logging
 
-log.getLogger('log')
-log_file = logging.FileHandler('log.log')
-log_file.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(message)s')
-log_file.setFormatter(formatter)
-log.addHandler(log_file)
+
 
 
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
-        log.info('WebSocket opened' + self.current_user )
+        print "WS opened"
         
 
     def on_message(self, message):
         self.write_message(message)
 
     def on_close(self):
-        log.info('WebSocket closed' )
+        print "closed"
         
 application = tornado.web.Application([
     (r"/", EchoWebSocket),
