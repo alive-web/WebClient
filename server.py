0dc9c74b5ae928ab657b9209210617928193036a
Vova <you@example.com>
2014-11-11 22:04:50 +0200

    server.py fixed

--- server.py
+++ server.py
@@ -2,12 +2,6 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import logging
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 70ef4814a647c44cc623cf899857447e92687338
 
 log = logging.getLogger('socket_log')
 log.setLevel(logging.INFO)
@@ -18,24 +12,18 @@ file_handler.setFormatter(formatter)
 log.addHandler(file_handler)
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
-<<<<<<< HEAD
         log.info("Connection opened")
-=======
         print "WS opened"
         
->>>>>>> 70ef4814a647c44cc623cf899857447e92687338
 
     def on_message(self, message):
     	log.info(message)
         self.write_message(message)
 
     def on_close(self):
-<<<<<<< HEAD
         log.info("Connection closed")
-=======
         print "closed"
-        
->>>>>>> 70ef4814a647c44cc623cf899857447e92687338
+
 application = tornado.web.Application([
     (r"/", EchoWebSocket),
 ])
