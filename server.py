941e762d781e7fabb8996931d47099b3b425a153
Vova <you@example.com>
2014-11-12 15:05:17 +0200

    -multimessage added, log removed

--- server.py
+++ server.py
@@ -3,27 +3,33 @@ import tornado.web
 import tornado.websocket
 import logging
 
-log = logging.getLogger('socket_log')
-log.setLevel(logging.INFO)
-file_handler = logging.FileHandler('web_socket.log')
-file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(message)s')
-file_handler.setFormatter(formatter)
-log.addHandler(file_handler)
+#log = logging.getLogger('socket_log')
+#log.setLevel(logging.INFO)
+#file_handler = logging.FileHandler('web_socket.log')
+#file_handler.setLevel(logging.INFO)
+#formatter = logging.Formatter('%(asctime)s - %(message)s')
+#file_handler.setFormatter(formatter)
+#log.addHandler(file_handler)
+
 
 
 class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    clients = set()
     def open(self):
-        log.info("Connection opened")
+        self.clients.add(self)
+        #log.info("Connection opened")
         print "WS opened"
         
 
     def on_message(self, message):
-        log.info(message)
-        self.write_message(message)
+        #log.info(message)
+        msg = message
+        for con in self.clients:
+            con.write_message(mg)
+        #self.write_message(message)
 
     def on_close(self):
-        log.info("Connection closed")
+        #log.info("Connection closed")
         print "closed"
 
 application = tornado.web.Application([
